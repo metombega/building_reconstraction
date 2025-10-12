b87acd6fc651e5023b70ea6e047fe31563e7105a
metombega <metombega@gmail.com>
2025-10-12 18:58:18 -0400

    get angle mesurements function

--- grid_search_algorithm.py
+++ grid_search_algorithm.py
@@ -25,23 +25,42 @@ def get_grid(top_mesurements, left_mesurements):
 
 def create_rays(width, height, min_width=1, min_height=1):
     # top_rays_angle = math.tan(min_width/height)
-    rays = []
+    top_rays = []
     dist_between_top_rays = min_width/height
     for i in range((width-1)*height//min_width):
-        rays.append(((i*dist_between_top_rays + dist_between_top_rays/2 , 0), (i*dist_between_top_rays + dist_between_top_rays/2 + min_width, height)))
+        top_rays.append(((i*dist_between_top_rays + dist_between_top_rays/2 , 0), (i*dist_between_top_rays + dist_between_top_rays/2 + min_width, height)))
+    side_rays = []
     dist_between_left_rays = min_height/width
     for i in range((height-1)*width//min_height):
-        rays.append(((0, i*dist_between_left_rays + dist_between_left_rays/2), (width, i*dist_between_left_rays + dist_between_left_rays/2 + min_height)))
-    return rays
+        side_rays.append(((width, i*dist_between_left_rays + dist_between_left_rays/2), (0, i*dist_between_left_rays + dist_between_left_rays/2 + min_height)))
+    return top_rays, side_rays
     
+def get_angle_mesurements(top_rays, side_rays, building: Building):
+    top_rays_mesurements = []
+    for ray in top_rays:
+        top_rays_mesurements.append(count_crossings(ray, building.segments))
+    side_rays_mesurements = []
+    for ray in side_rays:
+        side_rays_mesurements.append(count_crossings(ray, building.segments))
+    return top_rays_mesurements, side_rays_mesurements
+
+def get_grid_result(top_ray_mesurements, side_ray_mesurements, width, height):
+    # moving through all points in the grid
+    for row in range(1, width):
+        for column in range (height-2, 0):
+            k_visibility_left = top_ray_mesurements[row - 1 + height]
+
 if __name__ == '__main__':
     building = Building(random.randint(3,6), random.randint(3,6))
-    building.fill_grid_randomly(0.7)
+    building.fill_grid_randomly(1)
     top_mesurements = get_top_mesurements(building)
     # print(top_mesurements)
     left_mesurements = get_left_mesurements(building)
     # print(left_mesurements)
-    rays = create_rays(building.width,building.height)
-    present_segments([building.segments, rays])
+    top_rays, side_rays = create_rays(building.width,building.height)
+    top_ray_mesurements, side_ray_mesurements = get_angle_mesurements(top_rays, side_rays, building)
+    
+    present_segments([building.segments, top_rays, side_rays])
+    # present_segments([building.segments])
     # get_ray_mesurements = 
     # building.present_grid()

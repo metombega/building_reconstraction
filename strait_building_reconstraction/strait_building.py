3524fe1a8c4d10d6d0dbe2a2698b9622fe5c076f
metombega <metombega@gmail.com>
2025-10-28 12:09:51 -0400

    random building bugfix and improvement

--- strait_building_reconstraction/strait_building.py
+++ strait_building_reconstraction/strait_building.py
@@ -3,9 +3,8 @@ workspace_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if workspace_root not in sys.path:
     sys.path.insert(0, workspace_root)
 
-import numpy as np
 import random
-from tools.geometric import point_to_line_dist, point_to_point_dist
+from tools.geometric import point_to_line_dist, lines_are_overlap
 from tools.segment_presentation import present_segments
 
 class StraitBuilding():
@@ -23,51 +22,10 @@ class StraitBuilding():
         self.add_frame()
 
         while(len(self.segments) < num_of_segments):
-            if random.random() < 0.3:
+            if random.random() < 0.4:
                 self.add_rand_seg()
-            elif random.random() < 0.8:
+            else:
                 self.add_rand_corner_seg()
-            else:
-                self.add_rand_t_seg()
-
-    def add_rand_t_seg(self):
-        seg_to_connect = random.choice(self.segments)
-        reverse = False
-        if seg_to_connect in self.horizontal_segments:
-            h = random.uniform(self.min_wall_length, self.height/2)
-            if seg_to_connect[1][0] - seg_to_connect[0][0] <= 2*self.min_wall_length:
-                return
-            x1 = random.uniform(seg_to_connect[0][0] + self.min_wall_length, seg_to_connect[1][0] - self.min_wall_length)
-            y1 = seg_to_connect[0][1]
-            x2 = x1
-            if random.random() < 0.5:
-                y2 = min(y1 + h, self.height)
-            else:
-                y2 = max(y1 - h, 0)
-                reverse = True
-            
-        else:
-            w = random.uniform(self.min_wall_length, self.width/2)
-            if seg_to_connect[1][1] - seg_to_connect[0][1] <= 2*self.min_wall_length:
-                return
-            x1 = seg_to_connect[0][0]
-            y1 = random.uniform(seg_to_connect[0][1] + self.min_wall_length, seg_to_connect[1][1] - self.min_wall_length)
-            if random.random() < 0.5:
-                x2 = min(x1 + w, self.width)
-            else:
-                x2 = max(x1 - w, 0)
-                reverse = True
-            y2 = y1
-        if reverse:
-            new_segment = [(x2,y2), (x1,y1)]
-        else:
-            new_segment = [(x1,y1), (x2,y2)]
-        if (self.seg_legal_check(new_segment)):
-            self.segments.append(new_segment)
-            if seg_to_connect in self.horizontal_segments:
-                self.vertical_segments.append(new_segment)
-            else:
-                self.horizontal_segments.append(new_segment)
     
     def add_rand_corner_seg(self):
         seg_to_connect = random.choice(self.segments)
@@ -127,67 +85,32 @@ class StraitBuilding():
     
     def add_frame(self):
         self.segments.append([(0,0), (self.width, 0)])
-        # self.horizontal_segments.append([(0,0), (self.width, 0)])
         self.frame_segments.append([(0,0), (self.width, 0)])
         self.segments.append([(self.width, 0), (self.width, self.height)])
-        # self.vertical_segments.append([(self.width, 0), (self.width, self.height)])
         self.frame_segments.append([(self.width, 0), (self.width, self.height)])
         self.segments.append([(self.width, self.height), (0,self.height)])
-        # self.horizontal_segments.append([(self.width, self.height), (0,self.height)])
         self.frame_segments.append([(self.width, self.height), (0,self.height)])
         self.segments.append([(0,self.height), (0,0)])
-        # self.vertical_segments.append([(0,self.height), (0,0)])
         self.frame_segments.append([(0,self.height), (0,0)])
 
     def seg_legal_check(self, new_segment):
-        # check if segment is not too close to another segment
+        # check if segment is not too close to another segment, or overlaps
         for seg in self.segments:
-            if (point_to_line_dist(seg[0], new_segment) < self.min_wall_length
-                and point_to_line_dist(seg[0], new_segment) != 0
-                or point_to_line_dist(seg[1], new_segment) < self.min_wall_length 
-                and point_to_line_dist(seg[1], new_segment) != 0
-                or point_to_line_dist(new_segment[0], seg) < self.min_wall_length 
-                and point_to_line_dist(new_segment[0], seg) != 0
-                or point_to_line_dist(new_segment[1], seg) < self.min_wall_length
-                and point_to_line_dist(new_segment[1], seg) != 0):
-                return False
+            if ((point_to_line_dist(seg[0], new_segment) < self.min_wall_length
+                and point_to_line_dist(seg[0], new_segment) != 0)
+                or (point_to_line_dist(seg[1], new_segment) < self.min_wall_length 
+                and point_to_line_dist(seg[1], new_segment) != 0)
+                or (point_to_line_dist(new_segment[0], seg) < self.min_wall_length 
+                and point_to_line_dist(new_segment[0], seg) != 0)
+                or (point_to_line_dist(new_segment[1], seg) < self.min_wall_length
+                and point_to_line_dist(new_segment[1], seg) != 0)
+                or lines_are_overlap(seg, new_segment)):
+                    return False
         return True
     
-    # def move_point_from_edge(self, x, y):
-    #     if x < self.min_wall_length:
-    #         if x < self.min_wall_length/2:
-    #             x = 0
-    #         else:
-    #             x = self.min_wall_length
-        
-    #     elif x > self.width - self.min_wall_length:
-    #         if x > self.width - self.min_wall_length/2:
-    #             x = self.width
-    #         else:
-    #             x = self.width - self.min_wall_length
-
-    #     if y < self.min_wall_length:
-    #         if y < self.min_wall_length/2:
-    #             y = 0
-    #         else:
-    #             y = self.min_wall_length
-        
-    #     if y > self.height - self.min_wall_length:
-    #         if y > self.height - self.min_wall_length/2:
-    #             y = self.height
-    #         else:
-    #             y = self.height - self.min_wall_length
-    #     return x, y
-
-    # def frame_check(self, x1, x2, y1, y2):
-    #     # check segment is not too close to the frame
-    #     x1, y1 = self.move_point_from_edge(x1, y1)
-    #     x2, y2 = self.move_point_from_edge(x2, y2)
-        
-    #     return [(x1,y1), (x2,y2)]
 
 if __name__ == '__main__':
-    building = StraitBuilding(10,8)
+    building = StraitBuilding(8,10)
     building.create_random_building(14)
     print(building.segments)
     present_segments([building.segments])
--- tools/geometric.py
+++ tools/geometric.py
@@ -1,11 +1,10 @@
 from shapely.geometry import Point, LineString
+# import shapely
 
 def point_to_line_dist(p, line):
-    p = Point(p[0], p[1])
-    line = LineString([(line[0][0], line[0][1]), (line[1][0], line[1][1])])
-
+    p = Point(p)
+    line = LineString(line)
     distance = p.distance(line)
-    # print(distance)  # → 4.0
     return distance
 
 def point_to_point_dist(p1, p2):
@@ -13,5 +12,14 @@ def point_to_point_dist(p1, p2):
     p2 = Point(p2)
     return p1.distance(p2)
 
+def lines_are_overlap(line1, line2):
+    intersection = LineString(line1).intersection(LineString(line2))
+    return intersection.length > 0
+
 if __name__ == "__main__":
     print(point_to_line_dist((0,0), ((3,4),(6,6))))
+    line1 = [(3, 5), (5, 5)]
+    line2 = [(3, 5), (5, 5)]
+    intersection = LineString(line1).intersection(LineString(line2))
+    is_overlap = intersection.length > 0
+    print(is_overlap)
